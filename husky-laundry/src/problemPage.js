0e621230559cb5d375ba6a0628691bc7313ce804
gpalmer27 <palmer.gi@northeastern.edu>
2024-03-10 13:29:50 -0400

    fixed css elements

---
+++ husky-laundry/src/problemPage.js
@@ -0,0 +1,9 @@
+function returnRoom() {
+    let input = 
+    document.getElementById("roomNumber").value
+}
+
+function returnMachine() {
+    let input = 
+    document.getElementById("machineNumber").value
+}
