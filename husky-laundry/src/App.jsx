2b7f333610167c057bfa59d413645132efb795df
QW66666 <wusteven0829@gmail.com>
2024-03-23 17:18:05 -0400

    fixed merge conflict

--- husky-laundry/src/App.jsx
+++ husky-laundry/src/App.jsx
@@ -1,31 +1,10 @@
-<<<<<<< HEAD
-// src/App.js
+
+import { useState} from 'react'
 import React from 'react';
 import './App.css'; // Add any necessary styles
 import LaundryRoom from './LaundryRoom';
 import Problems from './Problems';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/rooms" element= {<LaundryRoom id = {1343672}/>} />
-          <Route path="/problems" element={<Problems/>} />  
-        </Routes>
-      </Router>
-     // {LaundryRoom(1343672)}
-  );
-}
-
-export default App;
-=======
-import { useState} from 'react'
-/* useState: state variable containing data retrieved from the backend
-  useEffect: fetch the backend on the first render */
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import MainPage from './MainPage'
 
 function App() {
   const [searchResult, setSearchResult] = useState([])
@@ -34,10 +13,11 @@ function App() {
     <Router>
     <Routes>
       <Route path="/" element= {<MainPage/>} />
+      <Route path="/rooms" element= {<LaundryRoom id = {1343672}/>} />
+          <Route path="/problems" element={<Problems/>} />  
     </Routes>
   </Router>
   )
 }
 
 export default App
->>>>>>> main
