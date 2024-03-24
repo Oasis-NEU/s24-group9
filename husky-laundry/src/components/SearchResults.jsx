63e3e7f6779fb025f1912a5e8044b917fe49dd92
QW66666 <wusteven0829@gmail.com>
2024-03-24 13:19:36 -0400

    fixed routing to room from search bar

--- husky-laundry/src/components/SearchResults.jsx
+++ husky-laundry/src/components/SearchResults.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./SearchResults.css"
 import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 export const SearchResults = ({result}) => {
-    const handleClick = () => {
-        axios.get("http://localhost:5050/location/id/" + result).then((response) => {
-          alert(response.data)
-        })
-    };
+  const navigateToRoom = useNavigate();
+  const handleClick = () => {
+      axios.get("http://localhost:5050/location/id/" + result).then((response) => {
+      navigateToRoom('/rooms', {state: response.data});
+    })
+  };
 
     return (
         <div 
