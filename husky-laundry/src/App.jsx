6ffa11aba3bd0ff2ebe3468ce3451f302ca70b93
QW66666 <wusteven0829@gmail.com>
2024-03-24 13:27:45 -0400

    fixed navigation to problems page

--- husky-laundry/src/App.jsx
+++ husky-laundry/src/App.jsx
@@ -2,10 +2,11 @@
 import { useState} from 'react'
 import React from 'react';
 import './App.css'; // Add any necessary styles
-import LaundryRoom from './LaundryRoom';
-import Problems from './ProblemPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './MainPage';
+import LaundryRoom from './LaundryRoom';
+import ProblemPage from './ProblemPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   const [searchResult, setSearchResult] = useState([])
@@ -15,7 +16,7 @@ function App() {
     <Routes>
       <Route path="/" element= {<MainPage/>} />
       <Route path="/rooms" element= {<LaundryRoom id = {1343672}/>} />
-          <Route path="/problems" element={<Problems/>} />  
+          <Route path="/problems" element={<ProblemPage/>} />  
     </Routes>
   </Router>
   )
--- husky-laundry/src/components/SearchResults.jsx
+++ husky-laundry/src/components/SearchResults.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SearchResults.css"
 import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 export const SearchResults = ({result}) => {
+  const navigateToRoom = useNavigate();
     const handleClick = () => {
         axios.get("http://localhost:5050/location/id/" + result).then((response) => {
-          alert(response.data)
+          navigateToRoom('/rooms', {state: response.data})
         })
     };
 
